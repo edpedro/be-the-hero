11779538818ff720a74ee8ce521014c3cb1e6a15
eduardo <edp2020.ep@gmail.com>
2020-03-28 21:51:02 -0300

    Validação e test na aplicação beck-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/OngController')
 const incidentController = require('./controllers/IncidentController')
@@ -9,13 +10,35 @@ const router = express.Router()
 
 router.post('/sessions', SessionController.create)
 
-router.post('/ongs', OngController.create)
+router.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create)
 router.get('/ongs', OngController.index)
 
-router.get('/profile', ProfileController.index)
+router.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+}), ProfileController.index)
 
 router.post('/incidents', incidentController.create)
-router.get('/incidents', incidentController.index)
-router.delete('/incidents/:id', incidentController.delete)
+
+router.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), incidentController.index)
+
+router.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), incidentController.delete)
 
 module.exports = router
